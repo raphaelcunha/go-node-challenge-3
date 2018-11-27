66ee963178ffba5a96fe40781230fb9ed255c6cc
Matheus Mondenez <matheus.mondenez@superlogica.com>
2018-11-27 08:40:30 -0200

    Conexão com mongo

---
+++ src/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    uri: 'mongodb://matheus:mp1193@ds159371.mlab.com:59371/node_marketplace'
+}
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const expres = require('express')
+const mongoose = require('mongoose')
+const dbConfig = require('./config/database')
 const routes = require('./routes')
 
 class App {
@@ -8,11 +10,19 @@ class App {
         this.isDev = process.env.NODE_ENV !== 'production'
 
         this.middlewares()
+        this.database()
         this.routes()
     }
 
     middlewares(){
         this.express.use(express.json())
+    }
+
+    database(){
+        mongoose.connect(dbConfig.uri, {
+            useCreateIndex: true,
+            useNewUrlParser: true
+        })
     }
 
     routes(){
