dc6131a8c498171b8acbbd78c52bf45553b980d4
Matheus Mondenez <matheusmondenez@gmail.com>
2018-11-27 22:22:36 -0200

    JWT

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../../config/auth");
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -19,6 +22,28 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-})
+});
 
-module.exports = mongoose.model('User', UserSchema)
+UserSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 8);
+});
+
+UserSchema.methods = {
+    compareHash(password) {
+        return bcrypt.compare(password, this.password);
+    }
+};
+
+UserSchema.statics = {
+    generateToken({ id }) {
+        return jwt.sign({ id }, authConfig.secret, {
+            expiresIn: authConfig.ttl
+        });
+    }
+};
+
+module.exports = mongoose.model("User", UserSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const routes = express.Router()
+const express = require("express");
+const routes = express.Router();
+const authMiddleware = require("./middlewares/auth");
 
-const UserController = require('./app/controllers/UserController')
+const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
 
-routes.post('/users', UserController.store)
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
-module.exports = routes
+routes.get("/teste", authMiddleware, (req, res) => res.json({ ok: true }));
+
+module.exports = routes;
